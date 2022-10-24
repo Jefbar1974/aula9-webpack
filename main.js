1f23e9495f41eeed957c764d53b229fc17088ffa
jefbar1974 <jefbar@gmail.com>
2022-10-24 19:43:43 -0300

    Iniciacio projeto NodeJS + Webpack

---
+++ main.js
@@ -0,0 +1,42 @@
+import "./style.scss";
+
+
+
+
+
+
+
+// document.querySelector("#botao_dividir").addEventListener("click", eldividir)
+
+// function eldividir() {
+//     const num_div1 = document.querySelector("#numero_dividir1")
+//     const num_div2 = document.querySelector("#numero_dividir2")
+
+//     const valor_div1 = Number(num_div1.value)
+//     const valor_div2 = Number(num_div2.value)
+    
+//     if (valor_div1 == "" || valor_div2 == "") {
+//         alert("Não pode valor vazio")
+//         return
+//     }
+//     if (valor_div2 == 0) {
+//         alert("Não pode valor zero")
+//         return
+//     }
+
+// }
+
+// const resultado_div = dividir(valor_div1, valor_div2)
+
+//     const resultado_div1 = document.querySelector("#resultado_div")
+
+//     resultado_div1.innerHTML = resultado_div
+
+//     function dividir(numero_dividir1, numero_dividir2) {
+//         const resultado_div = numero_dividir1 / numero_dividir2
+//         return resultado_div
+    
+          
+//     }
+
+   
